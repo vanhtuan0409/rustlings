7a16165cca441bb3010f7724eff9fa5c200fae66
Tuan Vuong <vanhtuan0409@gmail.com>
2020-06-17 17:25:14 +0700

    init

---
+++ standard_library_types/iterators4.rs
@@ -0,0 +1,37 @@
+// iterators4.rs
+
+pub fn factorial(num: u64) -> u64 {
+    match num {
+        0 => 1,
+        _ => num * factorial(num - 1),
+        // _ => (1..num + 1).fold(1, |mul, i| mul * i),
+    }
+    // Complete this function to return factorial of num
+    // Do not use:
+    // - return
+    // For extra fun don't use:
+    // - imperative style loops (for, while)
+    // - additional variables
+    // For the most fun don't use:
+    // - recursion
+    // Execute `rustlings hint iterators4` for hints.
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn factorial_of_1() {
+        assert_eq!(1, factorial(1));
+    }
+    #[test]
+    fn factorial_of_2() {
+        assert_eq!(2, factorial(2));
+    }
+
+    #[test]
+    fn factorial_of_4() {
+        assert_eq!(24, factorial(4));
+    }
+}
